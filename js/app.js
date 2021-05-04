ad56e64202b82020ba800c29661d783f05f979c3
raghavanfsd <info.rrao@gmail.com>
2021-05-04 13:26:06 +0530

    feat: Add landing page
    - A sample landing page whose navigation menu is built at run time using vanilla Javascript
    - Smooth scrolling to various sections is implemented
    - Distinguishing the various sections by highlighting the corresponding section and menu item while scrolling the page is implemented using the IntersectionObserver API
    - A button which helps the user to navigate to top of the page, smoothly has been added at the end of the page
    - Javascript functions including the helper functions are written as ES6 arrow functions

--- js/app.js
+++ js/app.js
@@ -17,41 +17,110 @@
  * Define Global Variables
  * 
 */
-
+const navBar = document.querySelector ("#navbar__list");
+const sectionList = document.querySelectorAll ("section");
+const sectionOffsetList = {};
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
+const buildSectionOffsetList = () => {
+  sectionList.forEach (section => {
+    sectionOffsetList [section.id] = Math.floor (section.getBoundingClientRect().top);
+  });
+};
 
+const getSectionOffset = (id) => {
+  return sectionOffsetList [id];
+};
 
+const buildLiHTML = (id, data) => {
+  return `<li id="li_${id}"><a id="li__a_${id}" href="#${id}">${data}</a></li>`;
+};
 
+const scrollToTop = (e) => {
+  /* Prevent the default behavior of the anchor hyperlinking */
+  e.preventDefault ();
+  window.scrollTo ( {left: 0, top: 0, behavior: 'smooth'} );
+};
+
+/* Scroll to section on link click */
+const navBarMenuClickEventHandler = (e) => {
+  /* Prevent the default behavior of the anchor hyperlinking */
+  e.preventDefault ();
+
+  let element = document.querySelector (e.target.hash);
+  let offset = getSectionOffset (element.id);
+
+  /* Scroll to anchor ID using scrollTO event */
+  window.scrollTo ( {left: 0, top: offset, behavior: 'smooth'} );
+}
+
+/* Add class 'active' to section when near top of viewport */
+const intersectionHandler = (entries) => {
+  entries.forEach (entry => {
+    let navItem = document.querySelector (`#li_${entry.target.id}`);
+    /* Remove the active class first. Add it only if the section is currently under View Port */
+    entry.target.classList.remove ("your-active-class");
+    navItem.classList.remove ("navbar__active");
+    if (entry ['isIntersecting'] === true) {
+      /* Set sections as active */
+      navItem.classList.add ("navbar__active");
+      entry.target.classList.add ("your-active-class");
+    }
+  });
+}
+
+const createIntersectionObserver = () => {
+  let observer;
+
+  let options = {
+    root: null,
+    rootMargin: '50px 0px 50px 0px',
+    threshold: [0.5]
+  };
+
+  observer = new IntersectionObserver (intersectionHandler, options);
+
+  sectionList.forEach (section => {
+    observer.observe (section);
+  });
+}
+
+const buildNavigation = () => {
+  /* Iterate through sectionList and add the li items to navBar, dynamically */
+  for(let section of sectionList) {
+    /* Get the section Id */
+    let sectionId = section.id;
+    /* Get the section data */
+    let sectionData = section.dataset.nav;
+
+    /* Build menu */
+    /* create the new li element */
+    let navBarItem = document.createElement ("li");
+    navBarItem.innerHTML = buildLiHTML (sectionId, sectionData);
+  
+    /* Now, insert the li item to navBar */
+    navBar.insertAdjacentElement ('beforeend', navBarItem);
+  }
+
+  createIntersectionObserver ();
+};
 /**
  * End Helper Functions
  * Begin Main Functions
  * 
 */
 
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
+/* build the nav */
+buildNavigation ();
+buildSectionOffsetList ();
 /**
  * End Main Functions
  * Begin Events
  * 
 */
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
+navBar.addEventListener ('click', navBarMenuClickEventHandler);
+document.querySelector ("#button__scroll_to_top").addEventListener ('click', scrollToTop);
